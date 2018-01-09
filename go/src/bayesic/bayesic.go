32ba696eedb8e0f6972481a6970b72f11f5e4ee1
Michael Ries <michael@riesd.com>
2018-01-09 10:32:59 -0700

    go fmt and cleanup a few iterations/conditionals

--- go/src/bayesic/bayesic.go
+++ go/src/bayesic/bayesic.go
@@ -1,55 +1,54 @@
 package bayesic
 
 type Matcher struct {
-  classifications stringSet
-  tokensByClassification map[string]stringSet
-  classificationsByToken map[string]stringSet
+	classifications        stringSet
+	tokensByClassification map[string]stringSet
+	classificationsByToken map[string]stringSet
 }
 
 type stringSet map[string]bool
 
-func New() Matcher {
-  return Matcher{
-    classifications: make(stringSet),
-    classificationsByToken: make(map[string]stringSet),
-    tokensByClassification: make(map[string]stringSet),
-  }
+func New() *Matcher {
+	return &Matcher{
+		classifications:        make(stringSet),
+		classificationsByToken: make(map[string]stringSet),
+		tokensByClassification: make(map[string]stringSet),
+	}
 }
 
 func (matcher *Matcher) Classify(tokens []string) map[string]float64 {
-  probabilities := make(map[string]float64)
-  for i := range tokens {
-    token := tokens[i]
-    _, exists := matcher.classificationsByToken[token]
-    if exists {
-      for class := range matcher.classificationsByToken[token] {
-        p_klass, exists := probabilities[class]
-        if !exists {
-          p_klass = 1.0 / float64(len(matcher.classifications))
-        }
-        p_not_klass := 1.0 - p_klass
-        p_token_given_klass := 1.0
-        p_token_given_not_klass := (float64(len(matcher.classificationsByToken[token])) - 1.0) / float64(len(matcher.classifications))
-        probabilities[class] = (p_token_given_klass * p_klass) / ((p_token_given_klass * p_klass) + (p_token_given_not_klass * p_not_klass))
-      }
-    }
-  }
-  return probabilities
+	probabilities := make(map[string]float64)
+	for _, token := range tokens {
+		_, exists := matcher.classificationsByToken[token]
+		if !exists {
+			continue
+		}
+		for class := range matcher.classificationsByToken[token] {
+			p_klass, exists := probabilities[class]
+			if !exists {
+				p_klass = 1.0 / float64(len(matcher.classifications))
+			}
+			p_not_klass := 1.0 - p_klass
+			p_token_given_klass := 1.0
+			p_token_given_not_klass := (float64(len(matcher.classificationsByToken[token])) - 1.0) / float64(len(matcher.classifications))
+			probabilities[class] = (p_token_given_klass * p_klass) / ((p_token_given_klass * p_klass) + (p_token_given_not_klass * p_not_klass))
+		}
+	}
+	return probabilities
 }
 
 func (matcher *Matcher) Train(tokens []string, class string) {
-  matcher.classifications[class] = true
-  _, exists := matcher.tokensByClassification[class]
-  if !exists {
-    matcher.tokensByClassification[class] = make(stringSet)
-  }
-  for idx := range tokens {
-    token := tokens[idx]
-    matcher.tokensByClassification[class][token] = true
-    _, exists := matcher.classificationsByToken[token]
-    if !exists {
-      matcher.classificationsByToken[token] = make(stringSet)
-    }
-    matcher.classificationsByToken[token][class] = true
-  }
+	matcher.classifications[class] = true
+	_, exists := matcher.tokensByClassification[class]
+	if !exists {
+		matcher.tokensByClassification[class] = make(stringSet)
+	}
+	for _, token := range tokens {
+		matcher.tokensByClassification[class][token] = true
+		_, exists := matcher.classificationsByToken[token]
+		if !exists {
+			matcher.classificationsByToken[token] = make(stringSet)
+		}
+		matcher.classificationsByToken[token][class] = true
+	}
 }
--- go/src/bayesic/bayesic_test.go
+++ go/src/bayesic/bayesic_test.go
@@ -2,36 +2,36 @@ package bayesic
 
 import "testing"
 
-func createMatcher() Matcher {
-  matcher := New()
-  matcher.Train([]string{"once","upon","a","time"}, "story")
-  matcher.Train([]string{"tonight","on","the","news"}, "news")
-  matcher.Train([]string{"it","was","the","best","of","times"}, "novel")
-  return matcher
+func createMatcher() *Matcher {
+	matcher := New()
+	matcher.Train([]string{"once", "upon", "a", "time"}, "story")
+	matcher.Train([]string{"tonight", "on", "the", "news"}, "news")
+	matcher.Train([]string{"it", "was", "the", "best", "of", "times"}, "novel")
+	return matcher
 }
 
 func TestClassifyMatchingTokens(t *testing.T) {
-  matcher := createMatcher()
-  classification := matcher.Classify([]string{"once","upon","a","time"})
-  probability := classification["story"]
-  if probability < 0.99 {
-    t.Errorf("expected the story probability to be over 99%%, but was %f", probability)
-  }
+	matcher := createMatcher()
+	classification := matcher.Classify([]string{"once", "upon", "a", "time"})
+	probability := classification["story"]
+	if probability < 0.99 {
+		t.Errorf("expected the story probability to be over 99%%, but was %f", probability)
+	}
 }
 
 func TestClassifyPartialMatch(t *testing.T) {
-  matcher := createMatcher()
-  classification := matcher.Classify([]string{"the","time"})
-  probability := classification["story"]
-  if probability < 0.9 {
-    t.Errorf("expected the story probability to be over 90%%, but was %f", probability)
-  }
+	matcher := createMatcher()
+	classification := matcher.Classify([]string{"the", "time"})
+	probability := classification["story"]
+	if probability < 0.9 {
+		t.Errorf("expected the story probability to be over 90%%, but was %f", probability)
+	}
 }
 
 func TestReturnsNoProbabilitiesForNonsense(t *testing.T) {
-  matcher := createMatcher()
-  classification := matcher.Classify([]string{"furby"})
-  if len(classification) > 0 {
-    t.Errorf("Expected no potential matches, but got %+v", classification)
-  }
+	matcher := createMatcher()
+	classification := matcher.Classify([]string{"furby"})
+	if len(classification) > 0 {
+		t.Errorf("Expected no potential matches, but got %+v", classification)
+	}
 }
