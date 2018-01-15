8718505d979497977729a7ed5067c822fb88a57a
Michael Ries <michael@riesd.com>
2018-01-15 11:15:36 -0700

    release 0.1.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Bayesic.Mixfile do
   def project do
     [
       app: :bayesic,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.5",
       start_permanent: Mix.env == :prod,
       package: package(),
