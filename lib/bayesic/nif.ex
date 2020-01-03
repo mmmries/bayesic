6cc49e55569876297a789913c8a1f9b8495e38ba
mmmries <riesmmm@gmail.com>
2020-01-03 08:20:27 -0700

    got the basics of the nif working

--- lib/bayesic/nif.ex
+++ lib/bayesic/nif.ex
@@ -2,5 +2,19 @@ defmodule Bayesic.Nif do
   use Rustler, otp_app: :bayesic, crate: "bayesic_nif"
 
   # When your NIF is loaded, it will override this function.
-  def add(_a, _b), do: :erlang.nif_error(:nif_not_loaded)
+  #def add(_a, _b), do: :erlang.nif_error(:nif_not_loaded)
+
+  def init, do: :erlang.nif_error(:nif_not_loaded )
+
+  def train(_bayesic, _class, _tokens) do
+    :erlang.nif_error(:nif_not_loaded)
+  end
+
+  def classify(_baysic, _tokens) do
+    :erlang.nif_error(:nif_not_loaded)
+  end
+
+  def prune(_bayesic, _threshold) do
+    :erlang.nif_error(:nif_not_loaded)
+  end
 end
--- native/bayesic_nif/src/lib.rs
+++ native/bayesic_nif/src/lib.rs
@@ -1,25 +1,94 @@
-use rustler::{Encoder, Env, Error, Term};
+use rustler::{Encoder, Env, NifResult, SchedulerFlags, Term};
+use rustler::resource::ResourceArc;
+use std::sync::Mutex;
+use bayesic::Bayesic;
 
 mod atoms {
     rustler::rustler_atoms! {
+        atom bad_reference;
+        atom error;
+        atom lock_fail;
         atom ok;
-        //atom error;
         //atom __true__ = "true";
         //atom __false__ = "false";
     }
 }
 
+pub struct BayesicResource(Mutex<Bayesic>);
+
 rustler::rustler_export_nifs! {
     "Elixir.Bayesic.Nif",
     [
-        ("add", 2, add)
+        ("init", 0, init),
+        ("train", 3, train),
+        ("classify", 2, classify, SchedulerFlags::DirtyCpu),
+        ("prune", 2, prune),
     ],
-    None
+    Some(load)
 }
 
-fn add<'a>(env: Env<'a>, args: &[Term<'a>]) -> Result<Term<'a>, Error> {
-    let num1: i64 = args[0].decode()?;
-    let num2: i64 = args[1].decode()?;
+fn load(env: Env, _info: Term) -> bool {
+  rustler::resource_struct_init!(BayesicResource, env);
+  true
+}
 
-    Ok((atoms::ok(), num1 + num2).encode(env))
+fn init<'a>(env: Env<'a>, _args: &[Term<'a>]) -> NifResult<Term<'a>> {
+  let resource = ResourceArc::new(BayesicResource(Mutex::new(Bayesic::new())));
+  Ok((atoms::ok(), resource).encode(env))
+}
+
+fn train<'a>(env: Env<'a>, args: &[Term<'a>]) -> NifResult<Term<'a>> {
+  let resource: ResourceArc<BayesicResource> = match args[0].decode() {
+    Err(_) => return Ok((atoms::error(), atoms::bad_reference()).encode(env)),
+    Ok(r) => r,
+  };
+  let class: String = args[1].decode()?;
+  let tokens: Vec<String> = args[2].decode()?;
+
+  let mut bayesic = match resource.0.try_lock() {
+    Err(_) => return Ok((atoms::error(), atoms::lock_fail()).encode(env)),
+    Ok(guard) => guard,
+  };
+
+  bayesic.train(class, tokens);
+
+  Ok(atoms::ok().encode(env))
+}
+
+fn classify<'a>(env: Env<'a>, args: &[Term<'a>]) -> NifResult<Term<'a>> {
+  let resource: ResourceArc<BayesicResource> = match args[0].decode() {
+    Err(_) => return Ok((atoms::error(), atoms::bad_reference()).encode(env)),
+    Ok(r) => r,
+  };
+  let tokens: Vec<String> = args[1].decode()?;
+
+  let mut bayesic = match resource.0.try_lock() {
+    Err(_) => return Ok((atoms::error(), atoms::lock_fail()).encode(env)),
+    Ok(guard) => guard,
+  };
+
+  let classification = bayesic.classify(tokens);
+  let mut result: Vec<(String, f64)> = vec!();
+  for (key, value) in classification {
+    result.push((key, value));
+  }
+
+  Ok((atoms::ok(), result).encode(env))
+}
+
+fn prune<'a>(env: Env<'a>, args: &[Term<'a>]) -> NifResult<Term<'a>> {
+  let resource: ResourceArc<BayesicResource> = match args[0].decode() {
+    Err(_) => return Ok((atoms::error(), atoms::bad_reference()).encode(env)),
+    Ok(r) => r,
+  };
+  let uniqueness_threshold: f64 = args[1].decode()?;
+
+  let mut bayesic = match resource.0.try_lock() {
+    Err(_) => return Ok((atoms::error(), atoms::lock_fail()).encode(env)),
+    Ok(guard) => guard,
+  };
+
+  bayesic.prune(uniqueness_threshold);
+
+  Ok(atoms::ok().encode(env))
 }
